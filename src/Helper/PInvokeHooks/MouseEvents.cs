99155f5e8797a274f22d34453169832101b3ab46
Maik Macho <nuda1998@gmail.com>
2017-03-01 04:36:04 +0100

    Fire mouse event messages (WM_MOUSEMOVE, WM_LBUTTONDOWN, ...)

--- src/Helper/PInvokeHooks/MouseEvents.cs
+++ src/Helper/PInvokeHooks/MouseEvents.cs
@@ -15,6 +15,33 @@ namespace XnaToFna {
         public static Rectangle? Clip;
         public static MouseState PreviousState;
 
+        public static void Moved()
+            => PInvokeHelper.CallHooks(Messages.WM_MOUSEMOVE, IntPtr.Zero, IntPtr.Zero);
+
+        public static void LMBDown()
+            => PInvokeHelper.CallHooks(Messages.WM_LBUTTONDOWN, IntPtr.Zero, IntPtr.Zero);
+        public static void LMBUp()
+            => PInvokeHelper.CallHooks(Messages.WM_LBUTTONUP, IntPtr.Zero, IntPtr.Zero);
+
+        public static void RMBDown()
+            => PInvokeHelper.CallHooks(Messages.WM_RBUTTONDOWN, IntPtr.Zero, IntPtr.Zero);
+        public static void RMBUp()
+            => PInvokeHelper.CallHooks(Messages.WM_RBUTTONUP, IntPtr.Zero, IntPtr.Zero);
+
+        public static void MMBDown()
+            => PInvokeHelper.CallHooks(Messages.WM_MBUTTONDOWN, IntPtr.Zero, IntPtr.Zero);
+        public static void MMBUp()
+            => PInvokeHelper.CallHooks(Messages.WM_MBUTTONUP, IntPtr.Zero, IntPtr.Zero);
+
+        public static void XMBDown(int mb)
+            => PInvokeHelper.CallHooks(Messages.WM_XBUTTONDOWN, (IntPtr) (mb << 16), IntPtr.Zero);
+        public static void XMBUp(int mb)
+            => PInvokeHelper.CallHooks(Messages.WM_XBUTTONUP, (IntPtr) (mb << 16), IntPtr.Zero);
+
+        public static void Wheel(int scroll)
+            // Damn, FNA is even accurate to WHEEL_DELTA being 120!
+            => PInvokeHelper.CallHooks(Messages.WM_MOUSEWHEEL, (IntPtr) (scroll << 16), IntPtr.Zero);
+
         public static void Update() {
             MouseState state = Mouse.GetState();
 
@@ -36,7 +63,40 @@ namespace XnaToFna {
                 my -= window.Y;
                 if (mx != state.X || my != state.Y)
                     Mouse.SetPosition(mx, my);
+                state = Mouse.GetState();
             }
+
+            if (state.X != PreviousState.X ||
+                state.Y != PreviousState.Y)
+                Moved();
+
+            if (state.LeftButton == ButtonState.Pressed && PreviousState.LeftButton == ButtonState.Released)
+                LMBDown();
+            else if (state.LeftButton == ButtonState.Released && PreviousState.LeftButton == ButtonState.Pressed)
+                LMBUp();
+
+            if (state.RightButton == ButtonState.Pressed && PreviousState.RightButton == ButtonState.Released)
+                RMBDown();
+            else if (state.RightButton == ButtonState.Released && PreviousState.RightButton == ButtonState.Pressed)
+                RMBUp();
+
+            if (state.MiddleButton == ButtonState.Pressed && PreviousState.MiddleButton == ButtonState.Released)
+                MMBDown();
+            else if (state.MiddleButton == ButtonState.Released && PreviousState.MiddleButton == ButtonState.Pressed)
+                MMBUp();
+
+            if (state.XButton1 == ButtonState.Pressed && PreviousState.XButton1 == ButtonState.Released)
+                XMBDown(1);
+            else if (state.XButton1 == ButtonState.Released && PreviousState.XButton1 == ButtonState.Pressed)
+                XMBUp(1);
+
+            if (state.XButton2 == ButtonState.Pressed && PreviousState.XButton2 == ButtonState.Released)
+                XMBDown(2);
+            else if (state.XButton2 == ButtonState.Released && PreviousState.XButton2 == ButtonState.Pressed)
+                XMBUp(2);
+
+            if (state.ScrollWheelValue != PreviousState.ScrollWheelValue)
+                Wheel(state.ScrollWheelValue - PreviousState.ScrollWheelValue);
 
             PreviousState = Mouse.GetState();
         }
